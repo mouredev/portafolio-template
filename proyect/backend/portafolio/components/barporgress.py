1b54dea5dcf39f6842ff78252f3284ff2b51e0c2
edimez14 <edimez14@gmail.com>
2024-06-06 13:00:04 -0500

    delete the project folder

--- proyect/backend/portafolio/components/barporgress.py
+++
@@ -1,8 +0,0 @@
-import reflex as rx
-
-def bar_progress(label: str, value: int) -> rx.Component:
-    return rx.box(
-        rx.text(label),
-        rx.progress(value=value,),
-        width="100%",
-    )
