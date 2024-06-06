126b0a77190c5a96b1a17060aa8a40a8bb13f536
edimez14 <edimez14@gmail.com>
2024-06-06 16:54:31 -0500

    update file build.sh, delete folder PUBLIC and create file vercel.json

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
 reflex init
-reflex run --env prod
+reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,4 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portafolio",
-    api_url="https://edizon-leal.onrender.com",
-    frontend_port=3000,
-    backend_port=35599,
 )
