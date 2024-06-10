76435b9482740026045106840c55e0d10ec85b58
edimez14 <edimez14@gmail.com>
2024-06-10 11:45:51 -0500

    update file build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,4 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -rf backend
-reflex init
-reflex export --backend-only
-unzip backend.zip -d backend
-rm -f backend.zip
 deactivate
