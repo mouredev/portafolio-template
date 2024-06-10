32d56c36f4016222f76f5758fd5cb21d65668898
edimez14 <edimez14@gmail.com>
2024-06-10 10:28:27 -0500

    update file build.sh, delete file vercel.json, create branch render-branch

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -rf public
+rm -rf backend
 reflex init
-reflex export --frontend-only
-unzip frontend.zip -d public
-rm -f frontend.zip
+reflex export --backend-only
+unzip backend.zip -d backend
+rm -f backend.zip
 deactivate
